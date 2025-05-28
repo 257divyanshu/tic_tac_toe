50a30f5cb67e958bb3ca2fae316e270a99bdad6d
Divyanshu Sahu <257divyanshu@gmail.com>
2025-05-28 13:17:40 +0530

    feat : block futher moves after a winner is declared

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -7,25 +7,27 @@ import "react-toastify/dist/ReactToastify.css";
 import isWinner from '../../helpers/checkWinner';
 
 function Grid({ numberOfCards }) {
-    const [turn,setTurn] = useState(true); // true -> O false -> X
-    const [board,setBoard] = useState(Array(numberOfCards).fill("")); // true -> O false -> X
-    const [winner,setWinner] = useState(null);
+    const [turn, setTurn] = useState(true); // true -> O false -> X
+    const [board, setBoard] = useState(Array(numberOfCards).fill("")); // true -> O false -> X
+    const [winner, setWinner] = useState(null);
     function cardClickHandler(index) {
-        if(turn){
-            board[index] = "O";
+        if (!winner) {
+            if (turn) {
+                board[index] = "O";
+            }
+            else {
+                board[index] = "X";
+            };
+            const winnerPlayer = isWinner(board, turn ? "O" : "X");
+            if (winnerPlayer) {
+                setWinner(winnerPlayer);
+                toast.success(`Congratulations ${winnerPlayer}`);
+            }
+            setBoard(board);
+            setTurn(!turn);
         }
-        else{
-            board[index] = "X";
-        };
-        const winnerPlayer = isWinner(board,turn ? "O":"X");
-        if(winnerPlayer){
-            setWinner(winnerPlayer);
-            toast.success(`Congratulations ${winnerPlayer}`);
-        }
-        setBoard(board);
-        setTurn(!turn);
     }
-    function resetGame(){
+    function resetGame() {
         setBoard(Array(numberOfCards).fill(""));
         setWinner(null);
         setTurn(true);
@@ -72,10 +74,10 @@ function Grid({ numberOfCards }) {
                 {/* we want the only the card that is clicked to re-render with the new icon */}
                 {/* so we need to do something inside the Card component's onclick handler */}
 
-                {board.map((value,idx)=>{
-                    return <Card key={idx} player={value} onCardClick={cardClickHandler} index={idx}/>
+                {board.map((value, idx) => {
+                    return <Card key={idx} player={value} onCardClick={cardClickHandler} index={idx} />
                 })}
-                
+
 
 
             </div>
